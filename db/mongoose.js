777a08fac5fb50dc3cd42e86ccf5f356c8f0d835
Abdul Sabir <sabirold91@gmail.com>
2018-07-27 17:22:17 +0500

    blah blah

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://Abdul Sabir:prsnl98=me@ds243049.mlab.com:43049/demo';
-const url = MONGODB_URI;
+const url = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
