e7df332a63b71afa0fcb4fbfdc9b8bb26e7ec43b
Abdul Sabir <sabirold91@gmail.com>
2018-08-01 15:18:05 +0500

    made lots of changes

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const Users = mongoose.model('Users', {
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -20,7 +22,50 @@ const Users = mongoose.model('Users', {
         required: true,
         trim: true,
         minlength: 1
+    },
+
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
+
+UserSchema.statics.findByToken = function(token) {
+    var decoded;
+    try {
+        decoded = jwt.verify(token, 'mysecret');
     }
-})
+    catch(e) {
+        return Promise.reject();
+    }
+
+    return this.findOne({
+        "_id": decoded._id,
+        "tokens.access": "auth",
+        "tokens.token": token
+    });
+}
+
+UserSchema.methods.toJSON = function() {
+    return _.pick(this, ['_id','name', 'age', 'gender']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+    const access = 'auth';
+    const token = jwt.sign({_id: this._id.toHexString(), access}, 'mysecret').toString();
+
+    this.tokens.push({access, token});
+    return this.save().then(() => {
+        return token;
+    });
+};
+
+const Users = mongoose.model('Users', UserSchema)
 
 module.exports = { Users };
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { mongoose } = require('./db/mongoose');
 const { Users } = require('./models/user');
 const { ObjectID } = require('mongodb');
 
+
+/////////////////////////////////////////////////////////
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -19,13 +21,29 @@ app.post('/users', (req, res) => {
         gender: req.body.gender
     });
 
-    newUser.save().then((doc) => {
-        res.status(200).send(doc);
-    }, (err) => {
+    newUser.save().then(() => {
+        return newUser.generateAuthToken();
+    }).then(token => {
+        res.header('x-auth', token).status(200).send(newUser);
+    }).catch(err => {
         res.status(400).send(err);
     });
 });
 
+app.post('/users/me', (req, res) => {
+    const token = req.header('x-auth');
+
+    Users.findByToken(token).then(user => {
+        if(!user) {
+            return Promise.reject();
+        }
+
+        res.status(200).send(user);
+    }).catch(err => {
+        res.status(401).send();
+    })
+
+})
 
 app.get('/users', (req, res) => {
     Users.find().then((docs) => {
